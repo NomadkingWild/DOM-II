af5cf34a8d159c6befbd1a2fad77efd12e1712a9
NomadkingWild <67304919+NomadkingWild@users.noreply.github.com>
2020-08-05 18:59:08 -0400

    made some changes and attempted to make some of my other ones work that did not.

--- js/index.js
+++ js/index.js
@@ -1,42 +1,48 @@
-//blur//
-
-document.addEventListener('blur',(event)=>{
-    event.target.style.background = "";
-},true)
-
+//img removal//
+const contentDestination = document.querySelector('.content-destination')
+const images = document.querySelectorAll('img')
+contentDestination.addEventListener('copy', function () {
+  closingImage.style.display = 'none';
+})
+const closingImage = document.querySelector('.content-destination img')
 //mousedown//
 const headerSwap = document.querySelectorAll("h2");
-headerSwap.querySelectorAll("h2").forEach(event =>{
-  event.addEventListener("mousedown", ()=>{ 
-    event.style.color = "green";
+headerSwap.forEach(h2 =>{
+  h2.addEventListener("click", (event)=>{ 
+    event.target.style.color = "green";
   
     })
 })
 
 //dblclick//
 const paraPolka = document.querySelectorAll("p");
-paraPolka.querySelectorAll("p").forEach(el=>{
-    el.addEventListener("dblclick",()=>{
-        el.target.style.fontWeight= "bold"
+paraPolka.forEach(p =>{
+    p.addEventListener("dblclick",(el)=>{
+        el.target.style.fontSize= "2rem"
     })
 })
 //click//
 const navLinks = document.querySelectorAll('a.nav-link');
-navLinks.querySelectorAll('a.nav-link').forEach(el => {
-    el.addEventListener('click', () => {
-        el.style.color = 'orange';
+navLinks.forEach(link=> {
+    link.addEventListener('click', (el) => {
+        el.target.style.color = 'orange';
     })
 })
-//keydown//
-document.addEventListener("keydown", event =>{
-    if (event.key === "p"){
-        event.target.style.color = "blue";
+//full blackout//
+const paraPolka1 = document.querySelectorAll("p");
+paraPolka1.forEach(p =>{
+    p.addEventListener("dblclick",(el)=>{
+        el.target.style.color = "black"
+        el.target.style.backgroundColor = "black"
+    })
+})
+//Blackout//
+document.addEventListener('keydown', event => {
+    if (event.key === 'd') {
+      document.body.style.color = "white"
+      document.body.style.backgroundColor = "black"
     }
-    })
-//scroll//
-window.addEventListener("scroll", ()=>{
-    event.target.scrolling.style ="yellow"
-})
+  })
 //keyup//
 document.addEventListener("keyup", event =>{
     if (event.key === "r"){
@@ -45,28 +51,28 @@ document.addEventListener("keyup", event =>{
         event.target.style.color="purple";
     }
     })
-    //onwheel//
-    scale = Math.min(Math.max(.125, scale), 4)
-    document.addEventListener("onwheel", event =>{
-        event.style.transform =`${scale}`
-    })
-    let scale =1;
-//resize//
-function sizeOut(){
-    event.preventDefault();
-    heightOutput.textContent = window.innerHeight
-    windowOutput.textContent = window. innerWidth
-}
-window.onresize = sizeOut;
-window.addEventListener("resize", sizeOut);
+//     //onwheel//
+//     scale = Math.min(Math.max(.125, scale), 4)
+//     .addEventListener("onwheel", event =>{
+//         event.style.transform =`${scale}`
+//     })
+//     let scale =1;
+// //resize//
+// function sizeOut(){
+//     event.preventDefault();
+//     heightOutput.textContent = window.innerHeight
+//     windowOutput.textContent = window. innerWidth
+// }
+// window.onresize = sizeOut;
+// window.addEventListener("resize", sizeOut);
 //select//
-function logSelection(event) {
-    const log = document.getElementById('log');
-    const selection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
-    log.textContent = `You selected: ${selection}`;
-  }
-const input = document.querySelector('input');
-input.addEventListener('select',logSelection);
+// function logSelection(event) {
+//     const log = document.getElementById('log');
+//     const selection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
+//     log.textContent = `You selected: ${selection}`;
+//   }
+// const input = document.querySelector('input');
+// input.addEventListener('select',logSelection);
 
 //propropgation//
 Array.from(document.all).forEach(el=>{
